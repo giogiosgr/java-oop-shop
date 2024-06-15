022d809898aa7b9e54c98b740ce094e6fa5ebe36
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-15 12:09:07 +0200

    feat: metodo getPrice

--- src/main/java/org/lessons/java/shop/Prodotto.java
+++ src/main/java/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,25 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+	private int code;
+	String name;
+	String description;
+	double price;
+	int tax;
+	
+    public Prodotto(String name, String description, double price, int tax) {
+    	//randomico compreso tra 1 e 99999999
+    	this.code = 1 + (int) (Math.random() * 99999999);
+    	this.name = name;
+    	this.description = description;
+    	this.price = price;
+    	this.tax = tax;
+    }
+    	
+    public double getPrice() {
+    	return this.price;	
+    }
+    	
+  
 
 }
